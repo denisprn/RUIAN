41f9056b8f9cd6d86ccf5f8df41c0e135d63c715
Denys <denis.peresichanskiy@gmail.com>
2021-06-25 10:23:45 +0200

    Add findBy methods

--- src/main/java/com/bp/RUIAN/repositories/AdresniMistoRepository.java
+++ src/main/java/com/bp/RUIAN/repositories/AdresniMistoRepository.java
@@ -1,6 +1,9 @@
 package com.bp.RUIAN.repositories;
 
 import com.bp.RUIAN.entities.AdresniMisto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -10,4 +13,15 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
  */
 @EnableElasticsearchRepositories
 public interface AdresniMistoRepository extends ElasticsearchRepository<AdresniMisto, Long> {
+    Page<AdresniMisto>
+            findAdresniMistosByUliceKodAndCisloDomovniLikeAndCisloOrientacniLikeAndCisloOrientacniPismenoLike(
+            Integer uliceKod, String cisloDomovni, String cisloOrientacni,
+            String cisloOrientacniPismeno, Pageable pageable);
+
+    @Query("{ \"bool\" : { \"must\" : [ " +
+            "{ \"query_string\" : { \"query\" : \"?0\", \"fields\" : [ \"uliceKod\" ] } } ], " +
+            "\"should\" : [ { \"query_string\" : { \"query\" : \"?1*\", \"fields\" : [ \"cisloDomovni\" ] , " +
+            "\"analyze_wildcard\": true } }, { \"query_string\" : { \"query\" : \"?1*\", " +
+            "\"fields\" : [ \"cisloOrientacni\" ], \"analyze_wildcard\": true } } ] } }")
+    Page<AdresniMisto> findByUliceKodWithCisloDomAndOrient(Integer uliceKod, String cislo, Pageable pageable);
 }
