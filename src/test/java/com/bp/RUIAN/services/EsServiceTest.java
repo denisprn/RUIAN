264f5af384d184856023448d9119c722eebf1087
denis <denis.peresichanskiy@gmail.com>
2021-09-27 17:27:21 +0200

    Edit test methods

--- src/test/java/com/bp/RUIAN/services/EsServiceTest.java
+++ src/test/java/com/bp/RUIAN/services/EsServiceTest.java
@@ -1,14 +1,19 @@
 package com.bp.RUIAN.services;
 
+import com.bp.RUIAN.entities.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,58 +23,124 @@ public class EsServiceTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Should return search result of street and number")
-    public void result1() throws Exception {
-        String result = """
-                [
-                    "<b>Masarykovo náměstí 18/16</b><br>Adresa, Jihlava",
-                    "<b>Masarykovo náměstí 67/14</b><br>Adresa, Jihlava",
-                    "<b>Masarykovo náměstí 16/65A</b><br>Adresa, Jihlava"
-                ]""";
+    @DisplayName("Should return search result of full given address")
+    public void resultOfFullGivenAddress() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> listExpected = new ArrayList<>();
+        Obec obec = Obec.create();
+        Ulice ulice = Ulice.create();
+        AdresniMisto adresniMisto = AdresniMisto.create();
+        Unit unitExpected = new Unit(adresniMisto, ulice, obec);
+        listExpected.add(unitExpected);
 
-        this.mockMvc.perform(get("/ruian/search?query={query}", "masarykovo naměstí 1"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().json(result));
+        MvcResult result = this.mockMvc.perform(get("/ruian/search?query={query}",
+                        "jihlava masarykovo nam 65 16"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+        String expectedJson = mapper.writeValueAsString(listExpected);
+
+        Assertions.assertEquals(expectedJson, actualJson);
     }
 
     @Test
-    @DisplayName("Should return search result of street and numbers with the '/'")
-    public void result2() throws Exception {
-        String result = """
-                [
-                    "<b>Masarykovo náměstí 16/65A</b><br>Adresa, Jihlava"
-                ]""";
+    @DisplayName("Should return search result of street and address")
+    public void resultOfGivenStreetAndAddress() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> listExpected = new ArrayList<>();
+        Obec obec = Obec.create();
+        Ulice ulice = Ulice.create();
+        AdresniMisto adresniMisto = AdresniMisto.create();
+        Unit unitExpected = new Unit(adresniMisto, ulice, obec);
+        listExpected.add(unitExpected);
 
-        this.mockMvc.perform(get("/ruian/search?query={query}", "masarykovo naměstí 1/6A"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().json(result));
+        MvcResult result = this.mockMvc.perform(get("/ruian/search?query={query}",
+                        "masarykovo nam 65 16"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+        String expectedJson = mapper.writeValueAsString(listExpected);
+
+        Assertions.assertEquals(expectedJson, actualJson);
     }
 
     @Test
-    @DisplayName("Should return search result of street and numbers before and after the '/'")
-    public void result3() throws Exception {
-        String result = """
-                [
-                    "<b>Masarykovo náměstí 63/56</b><br>Adresa, Jihlava",
-                    "<b>Masarykovo náměstí 62/57</b><br>Adresa, Jihlava"
-                ]""";
+    @DisplayName("Should return search result of given municipality and street")
+    public void resultOfGivenMunicipalityAndStreet() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> listExpected = new ArrayList<>();
+        Obec obec = Obec.create();
+        Ulice ulice = Ulice.create();
+        Unit unitExpected = new Unit(null, ulice, obec);
+        listExpected.add(unitExpected);
 
-        this.mockMvc.perform(get("/ruian/search?query={query}", "masarykovo naměstí 6/5"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().json(result));
+        MvcResult result = this.mockMvc.perform(get("/ruian/search?query={query}",
+                        "jihlava masarykovo nam"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+        String expectedJson = mapper.writeValueAsString(listExpected);
+
+        Assertions.assertEquals(expectedJson, actualJson);
     }
 
     @Test
-    @DisplayName("Should return search result of street and number before the '/'")
-    public void result4() throws Exception {
-        String result = """
-                [
-                    "<b>Masarykovo náměstí 18/16</b><br>Adresa, Jihlava",
-                    "<b>Masarykovo náměstí 16/65A</b><br>Adresa, Jihlava"
-                ]""";
+    @DisplayName("Should return search result of given municipality")
+    public void resultOfGivenMunicipality() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> listExpected = new ArrayList<>();
+        Obec obec = Obec.create();
+        Unit unitExpected = new Unit(null, null, obec);
+        listExpected.add(unitExpected);
 
-        this.mockMvc.perform(get("/ruian/search?query={query}", "masarykovo naměstí 1/"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().json(result));
+        MvcResult result = this.mockMvc.perform(get("/ruian/search?query={query}",
+                        "jihlava"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+        String expectedJson = mapper.writeValueAsString(listExpected);
+
+        Assertions.assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    @DisplayName("Should return search result of given street")
+    public void resultOfGivenStreet() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> listExpected = new ArrayList<>();
+        Obec obec = Obec.create();
+        Ulice ulice = Ulice.create();
+        Unit unitExpected = new Unit(null, ulice, obec);
+        listExpected.add(unitExpected);
+
+        MvcResult result = this.mockMvc.perform(get("/ruian/search?query={query}",
+                        "masarykovo nam"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+        String expectedJson = mapper.writeValueAsString(listExpected);
+
+        Assertions.assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    @DisplayName("Should return search result of given municipality and address")
+    public void resultOfGivenMunicipalityAndAddress() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> listExpected = new ArrayList<>();
+        AdresniMisto adresniMisto = AdresniMisto.create();
+        Obec obec = Obec.create();
+        Ulice ulice = Ulice.create();
+        Unit unitExpected = new Unit(adresniMisto, ulice, obec);
+        listExpected.add(unitExpected);
+
+        MvcResult result = this.mockMvc.perform(get("/ruian/search?query={query}",
+                        "jihlava 65 16"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+
+        String actualJson = result.getResponse().getContentAsString();
+        String expectedJson = mapper.writeValueAsString(listExpected);
+
+        Assertions.assertEquals(expectedJson, actualJson);
     }
 }
