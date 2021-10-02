1d6301403c5cb481f1a9e2a8fb278b80632766c0
denis <denis.peresichanskiy@gmail.com>
2021-10-02 20:21:30 +0200

    Add json annotation and setters

--- src/main/java/com/bp/RUIAN/entities/Unit.java
+++ src/main/java/com/bp/RUIAN/entities/Unit.java
@@ -1,3 +1,25 @@
 package com.bp.RUIAN.entities;
 
-public record Unit(AdresniMisto adresniMisto, Ulice ulice, Obec obec) { }
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class Unit {
+    private final AdresniMisto adresniMisto;
+    private Ulice ulice;
+    private Obec obec;
+
+    public Unit(AdresniMisto adresniMisto, Ulice ulice, Obec obec) {
+        this.adresniMisto = adresniMisto;
+        this.ulice = ulice;
+        this.obec = obec;
+    }
+
+    public void setUlice(Ulice ulice) {
+        this.ulice = ulice;
+    }
+
+    public void setObec(Obec obec) {
+        this.obec = obec;
+    }
+}
