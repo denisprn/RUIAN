d6af2b81845b2fad0acfac7992169bb9d56ae3f6
denis <denis.peresichanskiy@gmail.com>
2021-11-05 00:07:21 +0200

    Add AddressRepository class

---
+++ src/main/java/com/bp/RUIAN/repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.bp.RUIAN.repositories;
+
+import com.bp.RUIAN.entities.Address;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@EnableElasticsearchRepositories
+public interface AddressRepository extends ElasticsearchRepository<Address, Long> {
+
+}
