4cb1b85391f8ab24c23b2dd261bed5610360436f
denis <denis.peresichanskiy@gmail.com>
2021-11-05 00:07:01 +0200

    Add AddressParser class

---
+++ src/main/java/com/bp/RUIAN/parsers/AddressParser.java
@@ -0,0 +1,46 @@
+package com.bp.RUIAN.parsers;
+
+import com.bp.RUIAN.entities.Address;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class AddressParser implements RecordParser<Address> {
+    final String[] line;
+    final SimpleDateFormat sdf;
+
+    public AddressParser(String[] line) {
+        this.line = line;
+        this.sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        this.sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+    }
+
+    @Override
+    public Address parse() throws ParseException {
+        Integer id = Integer.parseInt(line[0]);
+        Integer municipalityId = Integer.parseInt(line[1]);
+        String municipalityName = line[2];
+        Integer momcId = line[3].isEmpty() ? null : Integer.parseInt(line[3]);
+        String momcName = line[4].isEmpty() ? null : line[4];
+        Integer mopId = line[5].isEmpty() ? null : Integer.parseInt(line[5]);
+        String mopName = line[6].isEmpty() ? null :line[6];
+        Integer municipalityPartId = line[7].isEmpty() ? null : Integer.parseInt(line[7]);
+        String municipalityPartName = line[8].isEmpty() ? null : line[8];
+        Integer streetId = line[9].isEmpty() ? null : Integer.parseInt(line[9]);
+        String streetName = line[10].isEmpty() ? null : line[10];
+        String typeSO = line[11];
+        String houseNumber = line[12];
+        String houseReferenceNumber = line[13].isEmpty() ? null : line[13];
+        String houseReferenceSign = line[14].isEmpty() ? null : line[14];
+        String zipCode = line[15];
+        String yCoordinate = line[16];
+        String xCoordinate = line[17];
+        Date validSince = sdf.parse(line[18]);
+
+        return new Address(id, municipalityId, municipalityName, momcId, momcName, mopId, mopName,
+                municipalityPartId, municipalityPartName, streetId, streetName, typeSO, houseNumber,
+                houseReferenceNumber, houseReferenceSign, zipCode, yCoordinate, xCoordinate, validSince);
+    }
+}
