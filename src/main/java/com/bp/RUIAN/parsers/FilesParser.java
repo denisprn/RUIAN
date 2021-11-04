be2fe96e430faa9010cab43d2f8745f71631e267
denis <denis.peresichanskiy@gmail.com>
2021-11-05 00:00:14 +0200

    Add minor changes

--- src/main/java/com/bp/RUIAN/parsers/FilesParser.java
+++ src/main/java/com/bp/RUIAN/parsers/FilesParser.java
@@ -1,34 +1,12 @@
 package com.bp.RUIAN.parsers;
 
-import com.bp.RUIAN.entities.*;
-import com.bp.RUIAN.parsers.*;
-import com.bp.RUIAN.services.EsService;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 
-/**
- * Source directory parser class
- * <p>
- *     This class recursively parse all XML files in the given directory
- * </p>
- * <p>
- *     This class also save parsed records to repositories
- * </p>
- * @author Denys Peresychanskyi
- */
-public class FilesParser {
-    private final EsService esService;
-
-    public FilesParser(EsService esService) {
-        this.esService = esService;
-    }
-
-    public void walk(String directoryPath) {
+public interface FilesParser {
+    default void walk(String directoryPath, String fileExtension) throws FileNotFoundException, UnsupportedEncodingException {
         File root = new File(directoryPath);
         File[] list = root.listFiles();
 
@@ -36,121 +14,13 @@ public class FilesParser {
 
         for (File f : list) {
             if (f.isDirectory()) {
-                walk(f.getAbsolutePath());
+                walk(f.getAbsolutePath(), fileExtension);
             }
-            else if (f.getName().endsWith(".xml")) {
+            else if (f.getName().endsWith(fileExtension)) {
                 parseFile(f.getAbsolutePath());
             }
         }
     }
 
-    private void parseFile(String filePath) {
-        try {
-            File file = new File(filePath);
-
-            //Get Document Builder
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-
-            //Build Document
-            Document document = builder.parse(file);
-            document.getDocumentElement().normalize();
-
-            //get list of "Data" elements
-            NodeList nListDataNode = document.getElementsByTagName("vf:Data");
-
-            //get child nodes of "Data"
-            NodeList nodeListData = nListDataNode.item(0).getChildNodes();
-
-            for (int i = 0; i < nodeListData.getLength(); i++) {
-                if (nodeListData.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                    NodeList nodeL = nodeListData.item(i).getChildNodes();
-
-                    for (int j = 0; j < nodeL.getLength(); j++) {
-                        Node node = nodeL.item(j);
-                        if (node.getNodeType() == Node.ELEMENT_NODE) {
-                            Element element = (Element) node;
-
-                            switch (node.getNodeName()) {
-                                case "vf:Stat" -> {
-                                    Stat stat = new StatParser(element).parse();
-                                    esService.saveStat(stat);
-                                }
-                                case "vf:RegionSoudrznosti" -> {
-                                    RegionSoudrznosti regionSoudrznosti =
-                                            new RegionSoudrznostiParser(element).parse();
-                                    esService.saveRs(regionSoudrznosti);
-                                }
-                                case "vf:Vusc" -> {
-                                    Vusc vusc = new VuscParser(element).parse();
-                                    esService.saveVusc(vusc);
-                                }
-                                case "vf:Okres" -> {
-                                    Okres okres = new OkresParser(element).parse();
-                                    esService.saveOkres(okres);
-                                }
-                                case "vf:Orp" -> {
-                                    Orp orp = new OrpParser(element).parse();
-                                    esService.saveOrp(orp);
-                                }
-                                case "vf:Pou" -> {
-                                    Pou pou = new PouParser(element).parse();
-                                    esService.savePou(pou);
-                                }
-                                case "vf:Obec" -> {
-                                    Obec obec = new ObecParser(element).parse();
-                                    esService.saveObec(obec);
-                                }
-                                case "vf:SpravniObvod" -> {
-                                    SpravniObvod spravniObvod =
-                                            new SpravniObvodParser(element).parse();
-                                    esService.saveSO(spravniObvod);
-                                }
-                                case "vf:Mop" -> {
-                                    Mop mop = new MopParser(element).parse();
-                                    esService.saveMop(mop);
-                                }
-                                case "vf:Momc" -> {
-                                    Momc momc = new MomcParser(element).parse();
-                                    esService.saveMomc(momc);
-                                }
-                                case "vf:CastObce" -> {
-                                    CastObce castObce = new CastObceParser(element).parse();
-                                    esService.saveCastObce(castObce);
-                                }
-                                case "vf:KatastralniUzemi" -> {
-                                    KatastralniUzemi katastralniUzemi =
-                                            new KatastralniUzemiParser(element).parse();
-                                    esService.saveKatastralniUzemi(katastralniUzemi);
-                                }
-                                case "vf:Zsj" -> {
-                                    Zsj zsj = new ZsjParser(element).parse();
-                                    esService.saveZsj(zsj);
-                                }
-                                case "vf:Ulice" -> {
-                                    Ulice ulice = new UliceParser(element).parse();
-                                    esService.saveUlice(ulice);
-                                }
-                                case "vf:Parcela" -> {
-                                    Parcela parcela = new ParcelaParser(element).parse();
-                                    esService.saveParcela(parcela);
-                                }
-                                case "vf:StavebniObjekt" -> {
-                                    StavebniObjekt stavebniObjekt =
-                                            new StavebniObjektParser(element).parse();
-                                    esService.saveStavebniObjekt(stavebniObjekt);
-                                }
-                                case "vf:AdresniMisto" -> {
-                                    AdresniMisto adresniMisto = new AdresniMistoParser(element).parse();
-                                    esService.saveAdresniMisto(adresniMisto);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
+    void parseFile(String filePath) throws FileNotFoundException, UnsupportedEncodingException;
 }
