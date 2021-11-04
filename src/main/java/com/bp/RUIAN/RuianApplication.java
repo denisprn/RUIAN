a8215a43d5692901c50793ce0aa265102d3efc81
denis <denis.peresichanskiy@gmail.com>
2021-11-05 00:01:14 +0200

    Add minor changes

--- src/main/java/com/bp/RUIAN/RuianApplication.java
+++ src/main/java/com/bp/RUIAN/RuianApplication.java
@@ -1,6 +1,7 @@
 package com.bp.RUIAN;
 
-import com.bp.RUIAN.parsers.FilesParser;
+import com.bp.RUIAN.parsers.CSVFilesParser;
+import com.bp.RUIAN.parsers.XMLFilesParser;
 import com.bp.RUIAN.services.EsService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Main class
@@ -24,20 +26,11 @@ public class RuianApplication {
 		SpringApplication.run(RuianApplication.class, args);
 	}
 
-	/**
-	 * Main method for parsing XML file
-	 * @param esService Elasticsearch service
-	 */
-	@Bean
+	/*@Bean
 	CommandLineRunner runner(EsService esService) {
 		return args -> {
-			//Parse xml files and import to Elasticsearch
-			File xmlDirectory = new ClassPathResource("/xml/").getFile();
-			String xmlDirectoryPath = xmlDirectory.getAbsolutePath();
-
-			//Invoke custom files parsing class
-			FilesParser filesParser = new FilesParser(esService);
-			filesParser.walk(xmlDirectoryPath);
+			String csvDirectoryPath = new ClassPathResource("/csv/").getFile().getAbsolutePath();
+			new CSVFilesParser(esService).walk(csvDirectoryPath, ".csv");
 		};
-	}
+	}*/
 }
