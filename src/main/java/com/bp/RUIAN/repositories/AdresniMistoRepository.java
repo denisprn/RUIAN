4de351136f3aeb9d2a63d3957ae8ef45387be8bf
denis <denis.peresichanskiy@gmail.com>
2021-11-06 11:55:21 +0200

    Minor cleanups

--- src/main/java/com/bp/RUIAN/repositories/AdresniMistoRepository.java
+++ src/main/java/com/bp/RUIAN/repositories/AdresniMistoRepository.java
@@ -1,10 +1,6 @@
 package com.bp.RUIAN.repositories;
 
 import com.bp.RUIAN.entities.AdresniMisto;
-import org.elasticsearch.common.unit.Fuzziness;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -14,33 +10,5 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
  */
 @EnableElasticsearchRepositories
 public interface AdresniMistoRepository extends ElasticsearchRepository<AdresniMisto, Long> {
-    /*Page<AdresniMisto> findAdresniMistosByUliceKodAndAndCisloDomovniLikeAndCisloOrientacniLikeAndCisloOrientacniPismenoLike(
-            Integer uliceKod, String cisloDomovni, String cisloOrientacni,
-            String cisloOrientacniPismeno, Pageable pageable);
 
-    Page<AdresniMisto> findAdresniMistosByUliceKodAndCisloDomovniLikeAndCisloOrientacniPismenoLike(
-            Integer uliceKod, String cisloDomovni, String cisloOrPismeno, Pageable pageable);
-
-    Page<AdresniMisto> findAdresniMistosByUliceKodAndCisloDomovniLike(
-            Integer uliceKod, String cisloDomovni, Pageable pageable);
-
-    Page<AdresniMisto> findAdresniMistosByCisloOrientacni(String cisloOrientacni, Pageable pageable);
-
-    Page<AdresniMisto> findAdresniMistosByUliceKodAndCisloOrientacniLikeAndCisloDomovniLike(
-            Integer uliceKod, String cisloOrientacni, String cisloDomovni, Pageable pageable);
-
-    Page<AdresniMisto> findAdresniMistosByUliceKodAndCisloOrientacniStartingWith(
-            Integer uliceKod, String cisloOrientacni, Pageable pageable);
-
-    Page<AdresniMisto>
-            findAdresniMistosByUliceKodAndCisloOrientacniLikeAndCisloDomovniLikeAndCisloOrientacniPismenoLike(
-            Integer uliceKod, String cisloOrientacni, String cisloDomovni,
-            String cisloOrientacniPismeno, Pageable pageable);
-
-    @Query("{ \"bool\" : { \"must\" : [ " +
-            "{ \"query_string\" : { \"query\" : \"?0\", \"fields\" : [ \"uliceKod\" ] } } ], " +
-            "\"should\" : [ { \"query_string\" : { \"query\" : \"?1*\", \"fields\" : [ \"cisloDomovni\" ] , " +
-            "\"analyze_wildcard\": true } }, { \"query_string\" : { \"query\" : \"?1*\", " +
-            "\"fields\" : [ \"cisloOrientacni\" ], \"analyze_wildcard\": true } } ] } }")
-    Page<AdresniMisto> findByUliceKodWithCisloDomOrOrient(Integer uliceKod, String cislo, Pageable pageable);*/
 }
