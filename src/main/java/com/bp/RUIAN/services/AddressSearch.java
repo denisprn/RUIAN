2a4b2b14d22ebb3850d4a8b616a953befc982987
denis <denis.peresichanskiy@gmail.com>
2021-11-07 15:57:01 +0200

    Add minor changes

--- src/main/java/com/bp/RUIAN/services/AddressSearch.java
+++ src/main/java/com/bp/RUIAN/services/AddressSearch.java
@@ -4,6 +4,7 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -28,9 +29,10 @@ public class AddressSearch {
         fields.put("municipalityName", 12.5F);
         fields.put("municipalityPartName", 14.5F);
         fields.put("streetName", 7.7F);
+        fields.put("houseIdentificationNumber", 9F);
         fields.put("houseNumber", 9F);
         fields.put("houseReferenceNumber", 6.5F);
-        fields.put("houseReferenceSign", 5.5F);
+        fields.put("houseReferenceSign", 10F);
         fields.put("zipCode", 7.5F);
 
         SearchRequest searchRequest = new SearchRequest("address");
@@ -39,7 +41,7 @@ public class AddressSearch {
                         .fields(fields)
                         .prefixLength(3)
                         .type(MultiMatchQueryBuilder.Type.MOST_FIELDS)
-                        .fuzziness(2))
+                        .fuzziness(Fuzziness.AUTO))
                 .size(5);
         searchRequest.source(searchSourceBuilder);
         SearchResponse searchResponse = esClient.search(searchRequest, RequestOptions.DEFAULT);
@@ -49,34 +51,21 @@ public class AddressSearch {
     }
 
     private String getHitsMainInfo(SearchHit searchHit) {
-        String address, identificationNumber;
+        String address;
         Map<String, Object> map = searchHit.getSourceAsMap();
         String municipalityName = map.get("municipalityName").toString();
         String municipalityPartName = map.get("municipalityPartName").toString();
         String zipCode = map.get("zipCode").toString();
-        String houseNumber = map.get("houseNumber").toString();
-
-        if (map.get("houseReferenceNumber") != null) {
-            String houseReferenceNumber = map.get("houseReferenceNumber").toString();
-
-            if (map.get("houseReferenceSign") != null) {
-                String houseReferenceSign = map.get("houseReferenceSign").toString();
-                identificationNumber = String.format("%s/%s%s", houseNumber, houseReferenceNumber, houseReferenceSign);
-            } else {
-                identificationNumber = String.format("%s/%s", houseReferenceNumber, houseNumber);
-            }
-        } else {
-            identificationNumber = houseNumber;
-        }
+        String houseIdentificationNumber = map.get("houseIdentificationNumber").toString();
 
         if (map.get("streetName") != null) {
             String streetName = map.get("streetName").toString();
             address = String.format("%s %s, %s %s, %s",
-                    streetName, identificationNumber, zipCode, municipalityPartName, municipalityName);
+                    streetName, houseIdentificationNumber, zipCode, municipalityPartName, municipalityName);
         } else {
             String typeSO = map.get("typeSO").toString();
             address = String.format("%s %s, %s %s, %s",
-                    typeSO, identificationNumber, zipCode, municipalityPartName, municipalityName);
+                    typeSO, houseIdentificationNumber, zipCode, municipalityPartName, municipalityName);
         }
 
         return address;
