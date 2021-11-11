e1c2305c579222e00d99ae66245b5631f3b23ccd
denis <denis.peresichanskiy@gmail.com>
2021-11-11 15:05:20 +0200

    Add minor changes

--- src/main/java/com/bp/RUIAN/RestClientConfig.java
+++ src/main/java/com/bp/RUIAN/RestClientConfig.java
@@ -18,7 +18,7 @@ public class RestClientConfig extends AbstractElasticsearchConfiguration {
     @Override
     public @NotNull RestHighLevelClient elasticsearchClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo("elasticsearch:9200")
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
