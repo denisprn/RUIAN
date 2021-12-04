215097e1441491c4fac0761d396811bcb3cdaea8
denis <denis.peresichanskiy@gmail.com>
2021-12-04 18:23:36 +0200

    Add minor changes

--- src/main/java/com/bp/RUIAN/RuianApplication.java
+++ src/main/java/com/bp/RUIAN/RuianApplication.java
@@ -1,28 +1,13 @@
 package com.bp.RUIAN;
 
-import com.bp.RUIAN.parsers.CSVFilesParser;
-import com.bp.RUIAN.services.EsService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.File;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RuianApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RuianApplication.class, args);
 	}
-
-	/*@Bean
-	CommandLineRunner runner(EsService esService) {
-		return args -> {
-			String csvDirectoryPath = new ClassPathResource(File.separator + "csv" + File.separator)
-					.getFile().getAbsolutePath();
-			new CSVFilesParser(esService).walk(csvDirectoryPath);
-		};
-	}*/
 }
