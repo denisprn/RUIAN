a6cb0b710645f784799c4f11ef88e87899da9c96
denis <denis.peresichanskiy@gmail.com>
2021-12-26 19:32:29 +0200

    Add Profile annotation

--- src/main/java/com/bp/RUIAN/ScheduledTasks.java
+++ src/main/java/com/bp/RUIAN/ScheduledTasks.java
@@ -2,10 +2,12 @@ package com.bp.RUIAN;
 
 import com.bp.RUIAN.services.CsvService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("dev")
 public class ScheduledTasks {
     @Autowired
     private CsvService csvService;
