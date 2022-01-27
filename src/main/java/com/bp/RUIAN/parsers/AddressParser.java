fce3ef8618041879c3f39bd2a6753de221adbb15
denis <denis.peresichanskiy@gmail.com>
2022-01-27 14:31:10 +0100

    Add check methods

--- src/main/java/com/bp/RUIAN/parsers/AddressParser.java
+++ src/main/java/com/bp/RUIAN/parsers/AddressParser.java
@@ -17,20 +17,28 @@ public class AddressParser implements RecordParser<Address> {
         this.sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
     }
 
+    private String checkIfEmptyString(String value) {
+        return value.isEmpty() ? null : value;
+    }
+
+    private Integer checkIfEmptyInteger(String value) {
+        return value.isEmpty() ? null : Integer.parseInt(value);
+    }
+
     @Override
     public Address parse() throws ParseException {
         String houseReferenceNumber, houseReferenceSign, houseIdentificationNumber;
         Integer id = Integer.parseInt(line[0]);
         Integer municipalityId = Integer.parseInt(line[1]);
         String municipalityName = line[2];
-        Integer momcId = line[3].isEmpty() ? null : Integer.parseInt(line[3]);
-        String momcName = line[4].isEmpty() ? null : line[4];
-        Integer mopId = line[5].isEmpty() ? null : Integer.parseInt(line[5]);
-        String mopName = line[6].isEmpty() ? null :line[6];
-        Integer municipalityPartId = line[7].isEmpty() ? null : Integer.parseInt(line[7]);
-        String municipalityPartName = line[8].isEmpty() ? null : line[8];
-        Integer streetId = line[9].isEmpty() ? null : Integer.parseInt(line[9]);
-        String streetName = line[10].isEmpty() ? null : line[10];
+        Integer momcId = checkIfEmptyInteger(line[3]);
+        String momcName = checkIfEmptyString(line[4]);
+        Integer mopId = checkIfEmptyInteger(line[5]);
+        String mopName = checkIfEmptyString(line[6]);
+        Integer municipalityPartId = checkIfEmptyInteger(line[7]);
+        String municipalityPartName = checkIfEmptyString(line[8]);
+        Integer streetId = checkIfEmptyInteger(line[9]);
+        String streetName = checkIfEmptyString(line[10]);
         String typeSO = line[11];
         String houseNumber = line[12];
 
