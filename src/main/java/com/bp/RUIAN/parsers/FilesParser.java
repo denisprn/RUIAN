2eb4e19edf805c47bee4c5fcfc1821a9455fb37b
denis <denis.peresichanskiy@gmail.com>
2022-02-05 14:53:28 +0100

    Delete EsService attribute

--- src/main/java/com/bp/RUIAN/parsers/FilesParser.java
+++ src/main/java/com/bp/RUIAN/parsers/FilesParser.java
@@ -1,20 +1,16 @@
 package com.bp.RUIAN.parsers;
 
 
-import com.bp.RUIAN.services.EsService;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 
 public abstract class FilesParser {
-    protected final EsService esService;
     protected final String fileExtension;
     protected final String fileEncoding = "windows-1250";
 
-    public FilesParser(EsService esService, String fileExtension) {
-        this.esService = esService;
+    public FilesParser(String fileExtension) {
         this.fileExtension = fileExtension;
     }
 
