bf059d8d16ce8623a247a795f638449daf12a494
denis <denis.peresichanskiy@gmail.com>
2022-02-11 10:38:02 +0100

    Add EsService variable

--- src/main/java/com/bp/RUIAN/parsers/FilesParser.java
+++ src/main/java/com/bp/RUIAN/parsers/FilesParser.java
@@ -1,16 +1,20 @@
 package com.bp.RUIAN.parsers;
 
 
+import com.bp.RUIAN.services.EsService;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 
 public abstract class FilesParser {
+    protected final EsService esService;
     protected final String fileExtension;
     protected final String fileEncoding = "windows-1250";
 
-    public FilesParser(String fileExtension) {
+    public FilesParser(EsService esService, String fileExtension) {
+        this.esService = esService;
         this.fileExtension = fileExtension;
     }
 
