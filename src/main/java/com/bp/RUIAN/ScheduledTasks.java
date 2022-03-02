3537bfa6b8be9ae2fe5e218697f5f600a0879246
denis <denis.peresichanskiy@gmail.com>
2022-03-02 19:25:32 +0100

    Edit class's param

--- src/main/java/com/bp/RUIAN/ScheduledTasks.java
+++ src/main/java/com/bp/RUIAN/ScheduledTasks.java
@@ -1,19 +1,25 @@
 package com.bp.RUIAN;
 
 import com.bp.RUIAN.services.CsvService;
+import com.bp.RUIAN.services.EsService;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 @Component
 @Profile("production")
 public class ScheduledTasks {
     @Autowired
-    private CsvService csvService;
+    private EsService esService;
 
     @Scheduled(cron = "${cron.value}", zone = "${cron.zone}")
-    public void updateData() {
-        csvService.updateData();
+    public void updateData() throws FileNotFoundException, UnsupportedEncodingException {
+        esService.updateData();
     }
 }
