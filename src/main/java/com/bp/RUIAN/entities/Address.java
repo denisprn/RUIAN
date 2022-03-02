bc627607a76c8168475ac7ca11e85329ef15c9aa
denis <denis.peresichanskiy@gmail.com>
2022-03-02 19:14:38 +0100

    Edit fields' params

--- src/main/java/com/bp/RUIAN/entities/Address.java
+++ src/main/java/com/bp/RUIAN/entities/Address.java
@@ -12,69 +12,95 @@ import java.util.Date;
 @Setting(settingPath = "es-config/elastic-analyzer.json")
 public record Address(
         @Id
-        @Field(type = FieldType.Integer, name = "id")
+        @Field(type = FieldType.Integer,
+                name = "id")
         Integer id,
 
-        @Field(type = FieldType.Integer, name = "municipalityId")
+        @Field(type = FieldType.Integer,
+                name = "municipalityId")
         Integer municipalityId,
 
-        @Field(type = FieldType.Text, name = "municipalityName", analyzer = "autocomplete_index",
-                searchAnalyzer = "autocomplete_search")
+        @Field(type = FieldType.Text,
+                name = "municipalityName",
+                analyzer = "autocompleteAnalyzer",
+                searchAnalyzer = "autocompleteAnalyzer")
         String municipalityName,
 
-        @Field(type = FieldType.Integer, name = "momcId")
+        @Field(type = FieldType.Integer,
+                name = "momcId")
         Integer momcId,
 
-        @Field(type = FieldType.Text, name = "momcName")
+        @Field(type = FieldType.Text,
+                name = "momcName")
         String momcName,
 
-        @Field(type = FieldType.Integer, name = "mopId")
+        @Field(type = FieldType.Integer,
+                name = "mopId")
         Integer mopId,
 
-        @Field(type = FieldType.Text, name = "mopName")
+        @Field(type = FieldType.Text,
+                name = "mopName")
         String mopName,
 
-        @Field(type = FieldType.Integer, name = "municipalityPartId")
+        @Field(type = FieldType.Integer,
+                name = "municipalityPartId")
         Integer municipalityPartId,
 
-        @Field(type = FieldType.Text, name = "municipalityPartName", analyzer = "autocomplete_index",
-                searchAnalyzer = "autocomplete_search")
+        @Field(type = FieldType.Text,
+                name = "municipalityPartName",
+                analyzer = "autocompleteAnalyzer",
+                searchAnalyzer = "autocompleteAnalyzer")
         String municipalityPartName,
 
-        @Field(type = FieldType.Integer, name = "streetId")
+        @Field(type = FieldType.Integer,
+                name = "streetId")
         Integer streetId,
 
-        @Field(type = FieldType.Text, name = "streetName", analyzer = "autocomplete_index",
-                searchAnalyzer = "autocomplete_search")
+        @Field(type = FieldType.Text,
+                name = "streetName",
+                analyzer = "autocompleteAnalyzer",
+                searchAnalyzer = "autocompleteAnalyzer")
         String streetName,
 
-        @Field(type = FieldType.Text, name = "typeSO")
+        @Field(type = FieldType.Text,
+                name = "typeSO")
         String typeSO,
 
-        @Field(type = FieldType.Text, name = "houseIdentificationNumber")
+        @Field(type = FieldType.Text,
+                name = "houseIdentificationNumber")
         String houseIdentificationNumber,
 
-        @Field(type = FieldType.Text, name = "houseNumber", analyzer = "autocomplete_index",
-                searchAnalyzer = "autocomplete_search")
+        @Field(type = FieldType.Text,
+                name = "houseNumber",
+                analyzer = "houseNumberOrReferenceNumberAnalyzer",
+                searchAnalyzer = "houseNumberOrReferenceNumberAnalyzer")
         String houseNumber,
 
-        @Field(type = FieldType.Text, name = "houseReferenceNumber", analyzer = "autocomplete_index",
-                searchAnalyzer = "autocomplete_search")
+        @Field(type = FieldType.Text,
+                name = "houseReferenceNumber",
+                analyzer = "houseNumberOrReferenceNumberAnalyzer",
+                searchAnalyzer = "houseNumberOrReferenceNumberAnalyzer")
         String houseReferenceNumber,
 
-        @Field(type = FieldType.Text, name = "houseReferenceSign")
+        @Field(type = FieldType.Text,
+                name = "houseReferenceSign",
+                analyzer = "houseReferenceSignAnalyzer",
+                searchAnalyzer = "houseReferenceSignAnalyzer")
         String houseReferenceSign,
 
-        @Field(type = FieldType.Text, name = "zipCode", analyzer = "autocomplete_index",
-                searchAnalyzer = "autocomplete_search")
+        @Field(type = FieldType.Text,
+                name = "zipCode")
         String zipCode,
 
-        @Field(type = FieldType.Text, name = "yCoordinate")
+        @Field(type = FieldType.Text,
+                name = "yCoordinate")
         String yCoordinate,
 
-        @Field(type = FieldType.Text, name = "xCoordinate")
+        @Field(type = FieldType.Text,
+                name = "xCoordinate")
         String xCoordinate,
 
-        @Field(type = FieldType.Date, name = "validSince")
+        @Field(type = FieldType.Date,
+                name = "validSince")
         Date validSince
 ) { }
