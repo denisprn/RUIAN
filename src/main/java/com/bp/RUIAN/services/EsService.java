73b9b2dc0453b4003662a967de699b068a916a8c
denis <denis.peresichanskiy@gmail.com>
2022-03-02 19:22:51 +0100

    Add updateData method

--- src/main/java/com/bp/RUIAN/services/EsService.java
+++ src/main/java/com/bp/RUIAN/services/EsService.java
@@ -1,32 +1,48 @@
 package com.bp.RUIAN.services;
 
 import com.bp.RUIAN.entities.*;
+import com.bp.RUIAN.parsers.AddressConverter;
 import com.bp.RUIAN.repositories.*;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 @Service
 public class EsService {
-    private final RestHighLevelClient esClient;
+    //private final RestHighLevelClient esClient;
+    private final AddressSearch addressSearch;
     private final AddressRepository addressRepository;
+    private final CsvService csvService;
 
-    public EsService(RestHighLevelClient esClient, AddressRepository addressRepository) {
-        this.esClient = esClient;
+    @Autowired
+    public EsService(/*RestHighLevelClient esClient*/AddressSearch addressSearch,
+                     AddressRepository addressRepository,
+                     CsvService csvService) {
+        //this.esClient = esClient;
+        this.addressSearch = addressSearch;
         this.addressRepository = addressRepository;
+        this.csvService = csvService;
     }
 
     public List<String> search(String searchString) throws IOException {
-        return new AddressSearch(esClient).search(searchString);
-    }
-
-    public void saveAddress(Address address) {
-        addressRepository.save(address);
+        return addressSearch.search(searchString);
     }
 
     public Optional<Address> findAddressById(Integer id) {
         return addressRepository.findById(id);
+    }
+
+    public void updateData() throws FileNotFoundException, UnsupportedEncodingException {
+        csvService.updateData();
     }
 }
