205ad406463cd877613c4347d0576918224358ef
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:44:45 +0200

    Implementation of AddressesImportScheduledTask class

---
+++ src/main/java/com/bp/ruian/scheduler/AddressesImportScheduledTask.java
@@ -0,0 +1,38 @@
+package com.bp.ruian.scheduler;
+
+import com.bp.ruian.config.AddressesImportScheduledTaskConfig;
+import com.bp.ruian.record.Address;
+import com.bp.ruian.service.EsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.CronTask;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * {@link Address} import scheduler
+ * @author denisprn
+ */
+@Component
+public class AddressesImportScheduledTask implements SchedulingConfigurer {
+    private final AddressesImportScheduledTaskConfig scheduledTaskConfig;
+    private final EsServiceImpl esServiceImpl;
+
+    @Autowired
+    public AddressesImportScheduledTask(AddressesImportScheduledTaskConfig scheduledTaskConfig,
+                                        EsServiceImpl esServiceImpl) {
+        this.scheduledTaskConfig = scheduledTaskConfig;
+        this.esServiceImpl = esServiceImpl;
+    }
+
+    private void importData() {
+        esServiceImpl.importAddresses();
+    }
+
+    @Override
+    public void configureTasks(@NonNull ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addCronTask(new CronTask(this::importData, scheduledTaskConfig.getCronTrigger()));
+    }
+}
