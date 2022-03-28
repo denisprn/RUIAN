eb1fa416dcdca66ba56398bc225451fdb90ea804
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:53:06 +0200

    Implementation of FileExtensions utility class

---
+++ src/main/java/com/bp/ruian/utils/FileExtensions.java
@@ -0,0 +1,14 @@
+package com.bp.ruian.utils;
+
+/**
+ * Utility class
+ * <p>Stores files' extension names</p>
+ * @author denisprn
+ */
+public final class FileExtensions {
+    public static final String CSV = "csv";
+
+    private FileExtensions() {
+        throw new IllegalStateException("Utility class");
+    }
+}
