034558c53c1629d3b5b954b7623c968ff4a82b5b
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:51:39 +0200

    Implementation of EsAnalyzerNames utility class

---
+++ src/main/java/com/bp/ruian/utils/EsAnalyzerNames.java
@@ -0,0 +1,19 @@
+package com.bp.ruian.utils;
+
+/**
+ * Utility class
+ * <p>Stores names of elasticsearch configuration file's analyzers</p>
+ * @author denisprn
+ */
+public final class EsAnalyzerNames {
+    public static final String AUTOCOMPLETE_INDEX = "autocompleteIndexAnalyzer";
+    public static final String AUTOCOMPLETE_SEARCH = "autocompleteSearchAnalyzer";
+    public static final String HOUSE_NUMBERS = "houseNumbersAnalyzer";
+    public static final String HOUSE_REFERENCE_SIGN = "houseReferenceSignAnalyzer";
+    public static final String ZIP_CODE = "zipCodeAnalyzer";
+    public static final String STANDARD = "standard";
+
+    private EsAnalyzerNames() {
+        throw new IllegalStateException("Utility class");
+    }
+}
