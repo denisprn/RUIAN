b233ba1762fae5b99b6731f65ab956d08d949f9f
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:54:12 +0200

    Implementation of MapUtils utility class

---
+++ src/main/java/com/bp/ruian/utils/MapUtils.java
@@ -0,0 +1,37 @@
+package com.bp.ruian.utils;
+
+import org.jetbrains.annotations.Nullable;
+import org.springframework.lang.NonNull;
+
+import java.util.Map;
+
+/**
+ * Utility class
+ * <p>Check/convert map values</p>
+ * @author denisprn
+ */
+public final class MapUtils {
+    @Nullable
+    public static String checkIfStringValueIsNotNull(@NonNull Map<String, Object> map, @NonNull String value) {
+        return map.get(value) != null ? map.get(value).toString() : null;
+    }
+
+    @Nullable
+    public static Integer checkIfIntegerValueIsNotNull(@NonNull Map<String, Object> map, @NonNull String value) {
+        return map.get(value) != null ? Integer.parseInt(map.get(value).toString()) : null;
+    }
+
+    @NonNull
+    public static String convertValueToString(@NonNull Map<String, Object> map, @NonNull String value) {
+        return map.get(value).toString();
+    }
+
+    @NonNull
+    public static Integer convertValueToInteger(@NonNull Map<String, Object> map, @NonNull String value) {
+        return Integer.parseInt(map.get(value).toString());
+    }
+
+    private MapUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+}
