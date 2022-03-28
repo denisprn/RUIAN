12427b38ff3f74d08e0faa1ff6451dd5a25ede5c
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:22:05 +0200

    Implementation of AddressConverter interface

---
+++ src/main/java/com/bp/RUIAN/converter/AddressConverter.java
@@ -0,0 +1,13 @@
+package com.bp.ruian.converter;
+
+import com.bp.ruian.record.Address;
+import org.elasticsearch.search.SearchHit;
+
+/**
+ * Interface for {@link Address} converting
+ * @author denisprn
+ */
+public interface AddressConverter {
+    Address convertFromArrayValues(String[] lineValues);
+    Address convertFromSearchHit(SearchHit searchHit);
+}
