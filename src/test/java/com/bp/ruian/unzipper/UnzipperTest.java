adbc5e98ab1d7784488239746c1948afc9f17402
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 21:48:12 +0200

    Implementation of Unzipper class's test

---
+++ src/test/java/com/bp/ruian/unzipper/UnzipperTest.java
@@ -0,0 +1,41 @@
+package com.bp.ruian.unzipper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests {@link UnzipperImpl} functionality
+ * @author denisprn
+ */
+@ContextConfiguration(classes = {
+        UnzipperImpl.class
+})
+@SpringBootTest
+class UnzipperTest {
+    @Autowired
+    private Unzipper unzipper;
+
+    @Test
+    @DisplayName("Test archive unzipping")
+    void unzip() {
+        final String addressesDirectoryPath = "./src/test/resources/addresses";
+        final String zipFilePath = "./src/test/resources/zip/Addresses.zip";
+
+        Path vresovaCsvFilePath = Paths.get("./src/test/resources/addresses/CSV/vresova.csv");
+        Path dobrohostCsvFilePath = Paths.get("./src/test/resources/addresses/CSV/dobrohost.csv");
+
+        unzipper.unzip(zipFilePath, addressesDirectoryPath);
+
+        assertThat(Files.exists(vresovaCsvFilePath)).isTrue();
+        assertThat(Files.exists(dobrohostCsvFilePath)).isTrue();
+    }
+}
