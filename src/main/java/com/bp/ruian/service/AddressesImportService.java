2e8accbcb3fa4614046d834da796b9fae82c1105
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:46:00 +0200

    Implementation of AddressesImportService interface

---
+++ src/main/java/com/bp/ruian/service/AddressesImportService.java
@@ -0,0 +1,9 @@
+package com.bp.ruian.service;
+
+/**
+ * Interface for address importing service
+ * @author denisprn
+ */
+public interface AddressesImportService {
+    void importAddressesFromRuianToEs();
+}
