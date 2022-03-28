685b811f43afb18aa4b08231f79ee3173b5c70b9
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:54:56 +0200

    Implementation of RestClientConfig class

---
+++ src/main/java/com/bp/ruian/config/RestClientConfig.java
@@ -0,0 +1,50 @@
+package com.bp.ruian.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.lang.NonNull;
+
+
+/**
+ * Elasticsearch REST client configuration
+ * @author denisprn
+ */
+@Configuration
+@ConfigurationProperties(prefix = "elastic.connection")
+public class RestClientConfig extends AbstractElasticsearchConfiguration {
+
+    private String host;
+
+    private String port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    @Bean
+    @Override
+    public @NonNull RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
