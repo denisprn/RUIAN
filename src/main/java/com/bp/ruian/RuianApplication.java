f203eb41db6115d499c6dd3d801707b7ddc1fdab
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:55:10 +0200

    Implementation of RuianApplication class

---
+++ src/main/java/com/bp/ruian/RuianApplication.java
@@ -0,0 +1,15 @@
+package com.bp.ruian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author denisprn
+ */
+@SpringBootApplication
+public class RuianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuianApplication.class, args);
+	}
+}
