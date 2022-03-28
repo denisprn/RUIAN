bebc4b93c795db8b921c812287eb5b590176fd82
Denys Peresychanskyi <denys.peresychanskyi@etnetera.cz>
2022-03-28 20:52:33 +0200

    Implementation of EsService class

---
+++ src/main/java/com/bp/ruian/service/EsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.bp.ruian.service;
+
+import com.bp.ruian.finder.AddressFinderImpl;
+import com.bp.ruian.record.Address;
+import com.bp.ruian.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Elasticsearch service
+ * @author denisprn
+ */
+@Service
+public class EsServiceImpl implements EsService {
+    private final AddressFinderImpl addressFinderImpl;
+    private final AddressRepository addressRepository;
+    private final AddressesImportServiceImpl addressesImportServiceImpl;
+
+    @Autowired
+    public EsServiceImpl(AddressFinderImpl addressFinderImpl,
+                         AddressRepository addressRepository,
+                         AddressesImportServiceImpl addressesImportServiceImpl) {
+        this.addressFinderImpl = addressFinderImpl;
+        this.addressRepository = addressRepository;
+        this.addressesImportServiceImpl = addressesImportServiceImpl;
+    }
+
+    @Override
+    public List<Address> findAddressesBySearchString(String searchString) {
+        return addressFinderImpl.find(searchString);
+    }
+
+    @Override
+    public Optional<Address> findAddressById(Integer id) {
+        return addressRepository.findById(id);
+    }
+
+    @Override
+    public void importAddresses() {
+        addressesImportServiceImpl.importAddressesFromRuianToEs();
+    }
+}
